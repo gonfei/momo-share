8f5f2c7f08ee227120a5fb3cc898e3c3fda03dee
iicovo <iicovo@163.com>
2021-12-25 14:45:06 +0800

    默认

--- auto-momo/ip.py
+++ auto-momo/ip.py
@@ -69,6 +69,7 @@ async def get_page(url, session, mod=0):
             async with await session.get(url=url, headers=hd, timeout=tout) as response:
                 page_source = await response.text()
                 await soup_page(page_source, mod=mod)
+                response.close()
     except Exception as e:
         print(f"['{url}']抓取失败:", e)
 
