9279bf76f506f8b914135b5df0566292b96eb8d3
iicovo <iicovo@163.com>
2021-12-25 14:41:50 +0800

    默认

--- auto-momo/momo.py
+++ auto-momo/momo.py
@@ -6,8 +6,8 @@ import asyncio
 from aiohttp import ClientSession, TCPConnector, ClientTimeout
 
 global n  # 记录访问成功次数
-link = 'link'  # 设置link
-# link = 'https://www.maimemo.com/share/page?uid=10229269&pid=8526dc258723264ef27890d668d2e391&tid=e33559031ac6fbe7eb4942c618b67c8b'
+# link = 'link'  # 设置link
+link = 'https://www.maimemo.com/share/page?uid=10229269&pid=8526dc258723264ef27890d668d2e391&tid=e33559031ac6fbe7eb4942c618b67c8b'
 
 # 如果检测到程序在 github actions 内运行，那么读取环境变量中的登录信息
 if environ.get('GITHUB_RUN_ID', None):
@@ -26,7 +26,7 @@ async def create_aiohttp(url, proxy_list):
 
 # 网页访问
 async def web_request(url, proxy, session):
-    tout = ClientTimeout(total=30)
+    tout = ClientTimeout(total=20)
     # 并发限制
     async with asyncio.Semaphore(3):
         try:
